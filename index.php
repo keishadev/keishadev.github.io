e95ad1113f5e6b0ad9dd7c5ba265f41271d3bd63
keishaatech <36748277+keishaatech@users.noreply.github.com>
2018-07-15 00:03:00 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title>Keisha developer</title>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8630117321167710",
+    enable_page_level_ads: true
+  });
+</script>
+    </head>
+<meta charset="UTF-8">
+<meta name="maValidation" content="edc700aae2ed4fec75a2ca4d2c481274" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+    background-image: url('https://w3schools.com/w3images/forestbridge.jpg');
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+}
+</style>
+<body>
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+    Logo
+  </div>
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top">Keisha Developer</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+    <p class="w3-large w3-center">Powered By <a href="https://geometrix168.blogspot.co.id/">GeoMetriX</a></p>
+  </div>
+  <div class="w3-display-bottomleft w3-padding-large">
+    Powered by <a href="https://geometrix168.blogspot.co.id/" target="_blank">Kei$ha</a>
+  </div>
+</div>
+<iframe data-aa='913157' src='//ad.a-ads.com/913157?size=728x15' scrolling='no' style='width:728px; height:15px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>
+</body>
+</html>
+
+<html>
+<head>
+<title>Welcome :)</title>
+<style type="text/css">
+html {
+    margin: 20px auto;
+    background:#111111;
+    color: green;
+    text-align: center;
+}
+pre {
+    color: white;
+}
+header {
+    color: green;
+    margin: 10px auto;
+}
+input[type=password] {
+    width: 200px;
+    height: 25px;
+    color: cyan;
+    background: #111111;
+    border: 1px;
+    padding: 5px;
+    margin-left: 20px;
+    text-align: center;
+}
+b {
+    color: cyan;
+}
+
+.kedip {
+-webkit-animation-name: blinker;
+-webkit-animation-duration: 3s;
+-webkit-animation-timing-function: linear;
+-webkit-animation-iteration-count: infinite;
+
+-moz-animation-name: blinker;
+-moz-animation-duration: 2s;
+-moz-animation-timing-function: linear;
+-moz-animation-iteration-count: infinite;
+
+ animation-name: blinker;
+ animation-duration: 1s;
+ animation-timing-function: linear;
+ animation-iteration-count: infinite;
+
+ color: white;
+}
+
+@-moz-keyframes blinker {  
+ 0% { opacity: 1.0; }
+ 50% { opacity: 0.0; }
+ 100% { opacity: 1.0; }
+ }
+
+@-webkit-keyframes blinker {  
+ 0% { opacity: 1.0; }
+ 50% { opacity: 0.0; }
+ 100% { opacity: 1.0; }
+ }
+
+@keyframes blinker {  
+ 0% { opacity: 1.0; }
+ 50% { opacity: 0.0; }
+ 100% { opacity: 1.0; }
+ }
+</style>
+</head>
+<center>
+<header>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+<pre onkeydown="return false;" onmousedown="return false;" class="kedip">
+    #############################################################    
+# d888888b d88888b db   dD  .d8b.  .d8888. db   db d888888b #
+# `~~88~~' 88'     88 ,8P' d8' `8b 88'  YP 88   88   `88'   #
+#    88    88ooooo 88,8P   88ooo88 `8bo.   88ooo88    88    #
+#    88    88~~~~~ 88`8b   88~~~88   `Y8b. 88~~~88    88    #
+#    88    88.     88 `88. 88   88 db   8D 88   88   .88.   #
+#    YP    Y88888P YP   YD YP   YP `8888Y' YP   YP Y888888P #
+#                                                           #
+#               [!] SITE UNDER CONSTRUCTION [!]             #
+#############################################################
+
+</pre>
+</body>
+<footer>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<pre onkeydown="return false;" onmousedown="return false;" class="kedip">
+Powered By TEKASHI | 2018 Clown Hacktivism 
+</pre>
+</br>
+</html>
